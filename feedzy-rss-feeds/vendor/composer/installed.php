9e3fa935e1dd99c96ba8f70305f08ba34d34bd47
Sean Murphy <sean@iamseanmurphy.com>
2026-09-07 15:53:26 +0000

    5.2.9

--- feedzy-rss-feeds/vendor/composer/installed.php
+++ feedzy-rss-feeds/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'codeinwp/feedzy-rss-feeds',
-        'pretty_version' => 'v5.2.8',
-        'version' => '5.2.8.0',
-        'reference' => '063c6a6b5f84c4de9b82aac16877640a9b146a35',
+        'pretty_version' => 'v5.2.9',
+        'version' => '5.2.9.0',
+        'reference' => '1d0b70f2bdedca170cb8c61324e320baa0bd1eb7',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,18 +11,18 @@
     ),
     'versions' => array(
         'codeinwp/feedzy-rss-feeds' => array(
-            'pretty_version' => 'v5.2.8',
-            'version' => '5.2.8.0',
-            'reference' => '063c6a6b5f84c4de9b82aac16877640a9b146a35',
+            'pretty_version' => 'v5.2.9',
+            'version' => '5.2.9.0',
+            'reference' => '1d0b70f2bdedca170cb8c61324e320baa0bd1eb7',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
             'dev_requirement' => false,
         ),
         'codeinwp/themeisle-sdk' => array(
-            'pretty_version' => '3.3.58',
-            'version' => '3.3.58.0',
-            'reference' => 'd6807c0b7308e323bd77cced667dee3f2d5e6a82',
+            'pretty_version' => '3.3.61',
+            'version' => '3.3.61.0',
+            'reference' => '9fe698b52dec768a0dd8b500fb51efe40962ee99',
             'type' => 'library',
             'install_path' => __DIR__ . '/../codeinwp/themeisle-sdk',
             'aliases' => array(),
